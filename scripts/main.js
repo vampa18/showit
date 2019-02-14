f7153fdd68e13d657689137d1ac452d449f8643b
vampa18 <ratisisvelashvili2016@gmail.com>
2019-02-14 18:30:14 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,4 @@
 
-$(document).ready(function() {
-	
-  var headerHeight = $('header').outerHeight(); // Target your header navigation here
-	
-  $('#main-nav li a').click(function(e) {
-  	
-  	var targetHref   = $(this).attr('href');
-	  
-	$('html, body').animate({
-		scrollTop: $(targetHref).offset().top - headerHeight // Add it to the calculation here
-	}, 1000);
-    
-    e.preventDefault();
-  });
-});
-
-
-
-
 jQuery(function($) {
   
   // Function which adds the 'animated' class to any '.animatable' in view
